48e5066733236eff303ec2ff0b60548a8a92be2b
regent-ndongmo <jauressndongmo5287@gmail.com>
2024-06-05 23:00:09 +0100

    5/06/2024

--- angular/src/app/Photographe/service/photographe.service.ts
+++ angular/src/app/Photographe/service/photographe.service.ts
@@ -2,16 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Photographe } from 'src/app/model/photographe/photographe';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotographeService{
 
+  private apiUrl = `${environment.apiUrl}/photographe`;
+
   constructor(private httpClient: HttpClient) { }
 
   getData(){
-    return this.httpClient.get("http://127.0.0.1:8000/api/Photographe");
+    return this.httpClient.get(`${this.apiUrl}`);
   }
 
   //insertion des donnees dans la Base de donnee
@@ -24,30 +27,31 @@ export class PhotographeService{
         formData.append(key, String(photographe[key]));
       }
     });
-    return this.httpClient.post("http://127.0.0.1:8000/api/Photographe", formData);
+    return this.httpClient.post(`${this.apiUrl}`, formData);
   }
 
   //Supprimer les donnees
   deleteData(id: Photographe){
-    return this.httpClient.delete('http://127.0.0.1:8000/api/photographe/'+id);
+    return this.httpClient.delete(`${this.apiUrl}/${id}`);
 
   }
 
   uploadImage(id: string, image: File): Observable<any> {
     const formData = new FormData();
     formData.append('photo', image);
-    return this.httpClient.put('http://127.0.0.1:8000/api/photographe/'+id, formData);
+    return this.httpClient.put(`${this.apiUrl}/${id}`, formData);
   }
 
   //Recuperer un employe en utilisant specifique en utilisant l'id
   getPhotographeById(id: Photographe){
-    return this.httpClient.get<Photographe>('http://127.0.0.1:8000/api/photographe/'+id);
+    return this.httpClient.get<Photographe>(`${this.apiUrl}/${id}`);
 
   }
 
   //Modifier l'Photographe selectionne (updatePhotographe)
-  changeProfile(id: Photographe, data: any) {
-    return this.httpClient.patch(`http://127.0.0.1:8000/api/photographe/${id}`, data);
+  changeProfile(data: any) {
+    // return this.httpClient.put(`${this.apiUrl}/${id}`, data);
+    return this.httpClient.post(`${this.apiUrl}/registerPhotographe`, data);
   }
 
   // registerPhotographe(data: any){
--- angular/src/environments/environment.development.ts
+++ angular/src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   baseUrl: 'http://localhost:8000/api/profile', // Votre base URL
   // imageBaseUrl: 'assets/' // Chemin relatif vers vos images
+  apiUrl: 'http://localhost:8000/api',
 
 };
