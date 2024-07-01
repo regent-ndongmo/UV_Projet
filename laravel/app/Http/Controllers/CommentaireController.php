5e85f27303cc703e9f6383befef617aeb8194241
franck-gif <franclaintomayo45@gmail.com>
2024-07-01 05:26:55 +0100

    mise a jour du controlleur ComentaireController pour la getion des crud sur un commentaire

--- laravel/app/Http/Controllers/CommentaireController.php
+++ laravel/app/Http/Controllers/CommentaireController.php
@@ -4,62 +4,92 @@ namespace App\Http\Controllers;
 
 use App\Models\Commentaire;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CommentaireController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the comments.
+     *
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        try {
+            $commentaires = Commentaire::with('photographe')->orderBy('created_at', 'desc')->get();
+            return response()->json($commentaires);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error retrieving comments', 'error' => $e->getMessage()], 500);
+        }
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Store a newly created comment in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'photographe_id' => 'required|exists:photographes,id',
+            'nom_client' => 'nullable|string',
+            'ville_client' => 'nullable|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        try {
+            $commentaire = Commentaire::create($request->all());
+            return response()->json(['message' => 'Comment created', 'comment' => $commentaire], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error creating comment', 'error' => $e->getMessage()], 500);
+        }
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Commentaire $commentaire)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Commentaire $commentaire)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
+     * Update the specified comment in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Commentaire  $commentaire
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Commentaire $commentaire)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'nom_client' => 'nullable|string',
+            'ville_client' => 'nullable|string',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 400);
+        }
+
+        try {
+            $commentaire->update($request->all());
+            return response()->json(['message' => 'Comment updated', 'comment' => $commentaire], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error updating comment', 'error' => $e->getMessage()], 500);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified comment from storage.
+     *
+     * @param  \App\Models\Commentaire  $commentaire
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy(Commentaire $commentaire)
     {
-        //
+        try {
+            $commentaire->delete();
+            return response()->json(['message' => 'Comment deleted'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Error deleting comment', 'error' => $e->getMessage()], 500);
+        }
     }
 }
