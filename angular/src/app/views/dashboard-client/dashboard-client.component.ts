e63494a3df54fcc67d05c5f18712e24ef42191b8
regent-ndongmo <jauressndongmo5287@gmail.com>
2024-07-07 07:44:58 +0100

    07/07/2024

--- angular/src/app/views/dashboard-client/dashboard-client.component.ts
+++ angular/src/app/views/dashboard-client/dashboard-client.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategorieService } from 'src/app/Photographe/service/Categorie/categorie.service';
+import { ImageService } from 'src/app/Photographe/service/image/image.service';
 
 @Component({
   selector: 'app-dashboard-client',
@@ -7,6 +9,32 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard-client.component.html',
   styleUrl: './dashboard-client.component.scss'
 })
-export class DashboardClientComponent {
+export class DashboardClientComponent implements OnInit {
+
+  categories : any;
+  images: any;
+
+  constructor(private serviceCategorie: CategorieService, private servicePhoto: ImageService){}
+
+
+  ngOnInit(): void {
+      this.getDataCategorie()
+      this.getDataPhoto()
+  }
+  getDataCategorie() {
+    this.serviceCategorie.getAll().subscribe(res => {
+      console.log(res);
+      this.categories = res;
+    })
+  }
+
+  getDataPhoto() {
+    this.servicePhoto.getAll().subscribe(res => {
+      console.log(res);
+      this.images = res;
+    })
+  }
+
+
 
 }
