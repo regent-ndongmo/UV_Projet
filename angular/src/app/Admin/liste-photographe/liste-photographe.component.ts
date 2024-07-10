a732332c77f472ee3575aa993b5e00f8b1bd9fb1
regent-ndongmo <jauressndongmo5287@gmail.com>
2024-07-10 19:18:35 +0100

    10/07/2024

--- angular/src/app/Admin/liste-photographe/liste-photographe.component.ts
+++ angular/src/app/Admin/liste-photographe/liste-photographe.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { GestionPhotographeService } from '../service/gestion-photographe.service';
+import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment.development';
+import { CommonModule } from '@angular/common';
 
+class Photo {
+  photographeRole: any
+}
 @Component({
   selector: 'app-liste-photographe',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './liste-photographe.component.html',
   styleUrl: './liste-photographe.component.scss'
 })
 export class ListePhotographeComponent implements OnInit{
 
   data : any;
+  imgURL: any;
+  photo = new Photo()
+  selectedRole: string = '';
 
   constructor(private servicePhotographe: GestionPhotographeService){}
 
@@ -19,10 +28,26 @@ export class ListePhotographeComponent implements OnInit{
   }
 
   getPhotographe(){
-    this.servicePhotographe.getAllUser().subscribe(res=>{
-      console.log(res)
+    this.servicePhotographe.getAllUser().subscribe(res => {
+      console.log(res);
       this.data = res;
-    })
+      this.data.forEach((item : any) => {
+        if (item.photo) {
+          // alert("ndjndjn"+ item.photo)
+          item.imgURL = `${environment.baseUrl}/${item.photo}`;
+        }
+      });
+    });
+  }
+
+  onRoleChange(event: Event) {
+    const selectElement = event.target as HTMLSelectElement;
+    this.selectedRole = selectElement.options[selectElement.selectedIndex].text;
+    console.log(this.selectedRole);
+  }
+
+  Appliquer(id: any, role: any){
+    console.log(id , role)
   }
 
 }
